6008cf90d81ce729a2819317471cde3359641da7
madhavi thatiparthi <madhavi609t@gmail.com>
2023-02-06 09:01:32 +0530

    first commit

---
+++ src/main/java/com/madhavishopping/App.java
@@ -0,0 +1,13 @@
+package com.madhavishopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
